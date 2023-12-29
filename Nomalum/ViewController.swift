16d99e4c5d2905ae28a74be8efb0661040421517
IOs-Dev-Diyor <diyorbek192004@icloud.com>
2023-12-29 13:01:28 +0500

    Initial Commit

---
+++ Nomalum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nomalum
+//
+//  Created by Diyorbek Xikmatullayev on 29/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
